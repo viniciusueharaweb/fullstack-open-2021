c48ec2dcf65628abe5a3f899d71e70759b0a2994
Vinicius Uehara <viuehara12@gmail.com>
2021-05-06 00:37:29 -0300

    add answer to exercise 1.2

--- part1/src/App.js
+++ part1/src/App.js
@@ -31,17 +31,19 @@ const Header = (props) => {
 
 const Content = (props) => {
     return (
-        <>
-            <p>
-                {props.part1} {props.exercises1}
-            </p>
-            <p>
-                {props.part2} {props.exercises2}
-            </p>
-            <p>
-                {props.part3} {props.exercises3}
-            </p>
-        </>
+        <div>
+            <Part part={props.part1} exercise={props.exercises1} />
+            <Part part={props.part2} exercise={props.exercises2} />
+            <Part part={props.part3} exercise={props.exercises3} />
+        </div>
+    );
+};
+
+const Part = (props) => {
+    return (
+        <p>
+            {props.part} {props.exercise}
+        </p>
     );
 };
 
