23589c2c7852b21177b1c647466641fc9b52b278
Vinicius Uehara <viuehara12@gmail.com>
2021-05-07 02:09:09 -0300

    add answer to exercise 1.5

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,27 +1,29 @@
 import React from "react";
 
 const App = () => {
-    const course = "Half Stack application development";
-    const parts = [
-        {
-            name: "Fundamentals of React",
-            exercises: 10,
-        },
-        {
-            name: "Using props to pass data",
-            exercises: 7,
-        },
-        {
-            name: "State of a component",
-            exercises: 14,
-        },
-    ];
+    const course = {
+        name: "Half Stack application development",
+        parts: [
+            {
+                name: "Fundamentals of React",
+                exercises: 10,
+            },
+            {
+                name: "Using props to pass data",
+                exercises: 7,
+            },
+            {
+                name: "State of a component",
+                exercises: 14,
+            },
+        ],
+    };
 
     return (
         <div>
-            <Header course={course} />
-            <Content parts={parts} />
-            <Total parts={parts} />
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
         </div>
     );
 };
@@ -31,10 +33,6 @@ const Header = (props) => {
 };
 
 const Content = (props) => {
-    console.log(props.parts);
-    // const firstExercisesNumber = props[0].exercises;
-    // const secondExercisesNumber = props[1].exercises;
-    // const thirdExercisesNumber = props[2].exercises;
     return (
         <div>
             <Part
